02740c3905419dab13dbb64eb702f17db7c16489
talbor49 <talbor49@gmail.com>
2019-03-29 14:42:46 +0300

    Abort on panic + manually define entry points

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Don't use standard library because it won't exist on our OS
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
@@ -13,4 +14,16 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 
-fn main() {}
+/*
+    Entry points for Windows - these are the function that will be first called.
+    Normally, the compiler generates them for us and calls our main() function,
+    but we don't use stdlib and the main wrapping, so we need to make them by ourselves
+*/
+#[no_mangle]
+pub extern "C" fn mainCRTStartup() -> ! {
+    main();
+}
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
