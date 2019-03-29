359298ed9aba79194376f31fb5bd16fa7e650cb6
talbor49 <talbor49@gmail.com>
2019-03-29 14:05:58 +0300

    No std - implement panic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// Don't use standard library because it won't exist on our OS
+#![no_std]
+
+use core::panic::PanicInfo;
+
+
+// Panic implementation is required because no standard lib
+// Diverging function marked by "never" type, never returns
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // On panic - halt the system
+    loop {}
 }
+
+
+fn main() {}
